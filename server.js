08063a907ec6d1244cdbc03d82133bf882b111b9
Mudith praveen <mudithpravin2@gmail.com>
2023-10-04 22:46:33 +0530

    start coding

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express'); //express import
+const mongoose = require('mongoose'); // import mongoose
+
+const app = express(); //express invok to our app
+
+const PORT = 8000; //running port
+
+const DB_URL = 'mongodb+srv://Admin:Admin@mernapp.ivdj90u.mongodb.net/mernCrud?retryWrites=true&w=majority';
+
+mongoose.connect(DB_URL)  //connnect db
+.then(() =>{  //if success
+    console.log('DB Connected');
+})
+.catch((err) =>{ // if not success
+    console.log('DB connection error',err);
+});
+
+app.listen(PORT, () =>{     //app is listen where want to run port
+    console.log(`App is running on ${PORT}`);
+});
+
