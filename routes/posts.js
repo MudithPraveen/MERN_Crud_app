c1a289beac1484b896795ccbfccf2c8a0793a10a
Mudith praveen <mudithpravin2@gmail.com>
2023-10-22 20:19:58 +0530

    get, update, delete function

--- routes/posts.js
+++ routes/posts.js
@@ -19,4 +19,55 @@ router.post('/post/save',(req,res)=>{
     });
 });
 
+
+//Get posts
+router.get('/posts',(req,res)=>{
+    Posts.find().exec((err,posts)=>{   //find is give by mongoose
+        if(err){
+            return res.status(400).json({
+                error:err
+            });
+        }
+        return res.status(200).json({
+            success:true,
+            existingPosts:posts
+        });
+    });
+});
+
+//Update posts
+router.put('post/update/:id',(req,res)=>{
+    Posts.findByIdAndUpdate(  //give by mongoose
+        req.params.id,
+        {
+            $set:req.body
+        },
+        (err,post)=>{
+            if(err){
+                return res.status(400).json({
+                    error:err
+                });
+            }
+            return res.status(200).json({
+                success:"Updated Successfully"
+            });
+        }
+    );
+});
+
+//Delete Posts
+router.delete('/post/delet:id',(req,res)=>{
+    Posts.findByIdAndRemove(req.params.id).exec((err,deletedPost)=>{
+        if(err){
+            return res.status(400).json({
+                message:"Delete Unsuccesful",err
+            });
+
+        }
+        return res.json({
+            message:"Delete Succesfull",deletedPost
+        });
+    });
+});
+
 module.exports = router;
